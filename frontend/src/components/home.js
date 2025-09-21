b5fb2a2518ed85c650b8c60ccc15710f76c8fa87
Nakul Nangru <nakulnangru2@gmail.com>
2025-09-21 18:08:34 +0530

    Create home.js

---
+++ frontend/src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <section>
+      <h2>Welcome, Indian Farmers!</h2>
+      <p>Track crops, check prices, get weather, and join the community.</p>
+    </section>
+  );
+}
+
+export default Home;
